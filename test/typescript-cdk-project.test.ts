495facc8d93effe96c2ea704f733f6ada9b1eb17
Kevin Mugele <kevin.mugele@gmail.com>
2023-09-19 11:00:37 -0400

    Initial commit

---
+++ test/typescript-cdk-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TypescriptCdkProject from '../lib/typescript-cdk-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/typescript-cdk-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TypescriptCdkProject.TypescriptCdkProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
