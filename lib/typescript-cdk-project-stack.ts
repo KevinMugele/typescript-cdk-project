f1f5a68586c9349efd3228c324789c2b88638233
Kevin Mugele <kevin.mugele@gmail.com>
2023-09-20 10:10:16 -0400

    Add comments in stack

--- lib/typescript-cdk-project-stack.ts
+++ lib/typescript-cdk-project-stack.ts
@@ -10,32 +10,38 @@ export class TypescriptCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Create S3 Bucket
     const s3Bucket = new s3.Bucket(this, 'kevin-colors-bucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       bucketName: "kevin-colors-bucket-2",
     });
 
+    // Create DynamoDB Table
     const dynamoTable = new dynamodb.Table(this, 'colorsDynamoDB', {
       tableName: "colorsDynamoDB",
       partitionKey: { name: 'color', type: dynamodb.AttributeType.STRING },
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
+    // Create Policy for accessing DynamoDB Table
     const dynamoAccessPolicy = new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
       actions: ['dynamodb:Query', 'dynamodb:Scan', 'dynamodb:GetItem', 'dynamodb:PutItem', 'dynamodb:UpdateItem', 'dynamodb:DeleteItem'],
       resources: [dynamoTable.tableArn],
     });
 
+    // Create IAM Role for Lambda
     const lambdaRole = new iam.Role(this, 'LambdaExecutionRole', {
       assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
     });
 
+    // Add policies to lambda role
     lambdaRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'));
     lambdaRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess'));
     lambdaRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AWSLambda_FullAccess'));
     lambdaRole.addToPolicy(dynamoAccessPolicy);
 
+    // Create Lambda
     const lambdaFunction = new lambda.Function(this, 'colorsLambda', {
       functionName: 'myColorsLambda',
       runtime: lambda.Runtime.NODEJS_16_X,
@@ -51,6 +57,7 @@ export class TypescriptCdkProjectStack extends cdk.Stack {
     s3Bucket.grantPut(lambdaFunction);
     dynamoTable.grantReadWriteData(lambdaFunction);
 
+    // Create trigger for lambda function
     lambdaFunction.addEventSource(new eventsources.S3EventSource(s3Bucket, {
       events: [s3.EventType.OBJECT_CREATED],
       filters: [{ suffix: '.json' }],
